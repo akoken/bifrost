2c70f5d4f24b4bdc2df18a9d76e2838756df32c3
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-08-24 11:18:52 +0300

    Use tokio for async operations

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
-use std::{
-    io::{BufRead, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:7000".to_string();
-    let listener = TcpListener::bind(&addr)?;
+    let listener = TcpListener::bind(&addr).await?;
+
     println!("Listening on: {}", addr);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let _ = handle_connection(stream);
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        println!("New connection from {}", stream.peer_addr()?);
+
+        // Handle the connection here
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(&mut stream).await {
+                println!("An error occurred: {}", e);
             }
-            Err(e) => {
-                println!("Connection failed:{}", e);
+        });
+    }
+}
+
+async fn handle_connection(stream: &mut TcpStream) -> std::io::Result<()> {
+    let mut buffer = vec![0; 1024];
+
+    loop {
+        match stream.read(&mut buffer).await {
+            // Return value of `Ok(0)` signifies that the remote has
+            // closed
+            Ok(0) => {
+                break;
+            }
+            Ok(n) => {
+                println!("Received:{:?}", String::from_utf8_lossy(&buffer[..n]));
+                if stream.write_all(&buffer[..n]).await.is_err() {
+                    break;
+                }
+            }
+            Err(_) => {
+                break;
             }
         }
     }
-
-    Ok(())
-}
-
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut reader = std::io::BufReader::new(&mut stream);
-    let request = reader.fill_buf()?.to_vec();
-    reader.consume(request.len());
-
-    let req = String::from_utf8_lossy(&request);
-    println!("Request:{}", &req);
-
-    stream.write_all(req.as_bytes())?;
-    stream.flush()?;
 
     Ok(())
 }
