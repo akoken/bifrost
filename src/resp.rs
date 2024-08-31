bcd8bedeca6a05bd10cb5bf7fd7a8b91d2fc155c
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-08-31 23:16:34 +0300

    Add resp parser types

---
+++ src/resp.rs
@@ -0,0 +1,29 @@
+const STRING: char = '+';
+const ERROR: char = '-';
+const INTEGER: char = ':';
+const BULK: char = '$';
+const ARRAY: char = '*';
+
+#[derive(Clone, Debug)]
+pub enum RespType {
+    BulkString(String),
+    SimpleString(String),
+    SimpleError(String),
+}
+
+#[derive(Debug)]
+pub enum RespError {
+    InvalidBulkString(String),
+    InvalidSimpleString(String),
+    Other(String),
+}
+
+impl std::fmt::Display for RespError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RespError::Other(msg) => msg.as_str().fmt(f),
+            RespError::InvalidBulkString(msg) => msg.as_str().fmt(f),
+            RespError::InvalidSimpleString(msg) => msg.as_str().fmt(f),
+        }
+    }
+}
