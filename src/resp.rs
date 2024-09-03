ba0bf2521c353951f68ba7717ded47c44263106c
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-09-03 21:55:21 +0300

    Implement array parsing

--- src/resp.rs
+++ src/resp.rs
@@ -1,3 +1,5 @@
+use std::{array, collections::btree_map::IterMut, usize};
+
 use bytes::{Bytes, BytesMut};
 
 const STRING: u8 = b'+';
@@ -11,6 +13,7 @@ pub enum RespType {
     BulkString(String),
     SimpleString(String),
     Integer(i64),
+    Array(Vec<RespType>),
     Error(String),
     Null,
 }
@@ -24,6 +27,13 @@ impl RespType {
             }
             RespType::SimpleString(ss) => Bytes::from_iter(format!("+{}\r\n", ss).into_bytes()),
             RespType::Integer(i) => Bytes::from(format!(":{}\r\n", i)),
+            RespType::Array(arr) => {
+                let mut result = format!("*{}\r\n", arr.len()).into_bytes();
+                for item in arr {
+                    result.extend_from_slice(&item.to_bytes());
+                }
+                Bytes::from(result)
+            }
             RespType::Error(es) => Bytes::from_iter(format!("-{}\r\n", es).into_bytes()),
             RespType::Null => Bytes::from("$-1\r\n"),
         }
@@ -36,6 +46,7 @@ impl PartialEq for RespType {
             (RespType::BulkString(a), RespType::BulkString(b)) => a == b,
             (RespType::SimpleString(a), RespType::SimpleString(b)) => a == b,
             (RespType::Integer(a), RespType::Integer(b)) => a == b,
+            (RespType::Array(a), RespType::Array(b)) => a == b,
             (RespType::Error(a), RespType::Error(b)) => a == b,
             _ => false,
         }
@@ -81,6 +92,7 @@ impl Resp {
             BULK => Self::parse_bulk_string(self),
             STRING => Self::parse_simple_string(self),
             INTEGER => Self::parse_integer(self),
+            ARRAY => Self::parse_array(self),
             ERROR => Self::parse_error(self),
             _ => Err(RespError::Other(String::from("Invalid RESP data type!"))),
         }
@@ -116,6 +128,26 @@ impl Resp {
     fn parse_integer(&self) -> Result<(RespType, usize), RespError> {
         let (value, len) = self.parse_integer_value(1)?;
         Ok((RespType::Integer(value), len))
+    }
+
+    // *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+    fn parse_array(&self) -> Result<(RespType, usize), RespError> {
+        let (arr_size, mut len) = self.parse_integer_value(1)?;
+        if arr_size == -1 {
+            return Ok((RespType::Null, len));
+        }
+
+        let arr_size = arr_size as usize;
+
+        let mut array = Vec::with_capacity(arr_size);
+        for _ in 0..arr_size {
+            let mut resp = Resp::new(BytesMut::from(&self.buffer[len..]));
+            let (item, item_len) = resp.parse()?;
+            array.push(item);
+            len += item_len;
+        }
+
+        Ok((RespType::Array(array), len))
     }
 
     // -Error message\r\n
@@ -188,6 +220,19 @@ mod tests {
     fn test_parse_integer() {
         let mut resp = Resp::new(BytesMut::from(":23\r\n"));
         assert_resp_eq(resp.parse(), RespType::Integer(23), 5);
+    }
+
+    #[test]
+    fn test_parse_array() {
+        let mut resp = Resp::new(BytesMut::from("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"));
+        assert_resp_eq(
+            resp.parse(),
+            RespType::Array(vec![
+                RespType::BulkString("hello".to_string()),
+                RespType::BulkString("world".to_string()),
+            ]),
+            26,
+        );
     }
 
     #[test]
